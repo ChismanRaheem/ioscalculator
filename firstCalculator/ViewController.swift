6da52843f860b6b0bc25c634ea5b558e4845e40c
Raheem Chisman <raheem.chisman@microsoft.com>
2024-06-02 10:06:09 -0700

    firstcalc deployed

--- firstCalculator/ViewController.swift
+++ firstCalculator/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var secondText: UITextField!
+    
+    var result = 3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func addClicked(_ sender: Any) {
+        
+        if  let firstNumber = Int(firstText.text!){
+            if  let secondNumber = Int(secondText.text!){
+                
+                result = firstNumber + secondNumber
+                resultLabel.text = String(result)
+                
+                
+            }
+        }
+    }
+    
+    @IBAction func subtractClicked(_ sender: Any) {
+        if let firstNumber = Int(firstText.text!){
+            if let secondNumber = Int(secondText.text!){
+                
+                 result = firstNumber - secondNumber
+                resultLabel.text = String (result)
+            }
+        }
+    }
+    
+    @IBAction func multiplyClicked(_ sender: Any) {
+        if  let firstNumber = Int(firstText.text!){
+            if  let secondNumber = Int(secondText.text!){
+                
+                result = firstNumber * secondNumber
+                resultLabel.text = String (result)
+            }
+            
+        }
+    }
 }
-
